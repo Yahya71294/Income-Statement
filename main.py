7aa9bc2cb092e323916fbd75a36a8a2f5dfc5370
Yahya71294 <yahyaaltaf34@gmail.com>
2026-08-03 10:05:08 +0500

    Add tax collection and display functionality

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from income_statement import calculate_totals,display_statement
-from data_collect import income_collect, expense_collect
+from income_statement import calculate_totals,display_statement, tax_display
+from data_collect import income_collect, expense_collect, taxes_collect
 
 
 print("Welcome to the Income Statement Generator")
@@ -7,5 +7,7 @@ company_name = input("\nEnter your Company name:\n")
 year = int(input("\nEnter the year (e.g., 2026):\n"))
 income = income_collect()
 expense = expense_collect()
-total_income, total_expense, net_income = calculate_totals(income, expense)
+taxes = taxes_collect()
+total_income, total_expense, net_income,after_tax,net_tax = calculate_totals(income, expense,taxes)
 display_statement(company_name, income, expense, total_income, total_expense, net_income, year)
+tax_display(taxes, net_tax, after_tax)
