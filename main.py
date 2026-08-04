86520ae9c43b712b7e7a55dfb94472ddf957e9c7
Yahya71294 <yahyaaltaf34@gmail.com>
2026-08-04 18:54:17 +0500

    Include date input in income statement generation
    Added date input to the income statement generator and modified display function to include date.

--- main.py
+++ main.py
@@ -3,11 +3,12 @@ from data_collect import income_collect, expense_collect, taxes_collect
 
 
 print("Welcome to the Income Statement Generator")
+date =input("\nEnter Date:\n")
 company_name = input("\nEnter your Company name:\n")
 year = int(input("\nEnter the year (e.g., 2026):\n"))
 income = income_collect()
 expense = expense_collect()
 taxes = taxes_collect()
 total_income, total_expense, net_income,after_tax,net_tax = calculate_totals(income, expense,taxes)
-display_statement(company_name, income, expense, total_income, total_expense, net_income, year)
+display_statement(date,company_name, income, expense, total_income, total_expense, net_income, year)
 tax_display(taxes, net_tax, after_tax)
