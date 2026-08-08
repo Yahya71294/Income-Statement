a095f2913e8726644ead714364bfb3da1cf41976
Yahya71294 <yahyaaltaf34@gmail.com>
2026-08-08 15:06:00 +0500

    Refactor data collection functions with categories
    Enhanced income, expense, and tax collection functions with category selection and validation.

--- data_collect.py
+++ data_collect.py
@@ -2,11 +2,21 @@
 # To collect all the data related to income
 def income_collect():
     income ={}
+    income_category =["1. Sales Revenue","2. Service Revenue","3. Rental Income","4. Interest Income","5. Commission Income","6. Other Income"]
 
     while True:
-        option = input("Type 'I' to add an income or 'D' to Finish:\n").lower()
+        option = input("\nType 'i' to add an income or 'd' to Finish as an option:\n").lower()
         if option == "i":
-            category = input("Enter Income Category:\n").lower()
+            print("Select an Income Category:\n")
+            for cat in income_category:
+                print(cat)
+            while True:
+                income_input = input("\nEnter Income Category:\n")
+                if income_input.replace(" ","").isalpha():
+                    category = income_input
+                    break
+                elif income_input != income_input.isalpha():
+                    print("Invalid Option")
             amount = float(input("Enter an Amount\n"))
             income[category] = amount
         elif option == "d":
@@ -17,28 +27,48 @@ def income_collect():
 # To collect all the data related to expenses
 def expense_collect():
     expense = {}
+    expense_category =['1. Rent','2. Salaries','3. Maintenance','4. Loan Payment','5. Utilities Expense','6. Other Expense']
     while True:
-        option = input("Type 'E' to add expenses or 'D' to Finish:\n").lower()
+        option = input("\nType 'e' to add expenses or 'd' to Finish as an option:\n").lower()
         if option == "e":
-           category = input("Enter Expense Category:\n").lower()
-           amount = float(input("Enter an Amount:\n"))
-           expense[category] = amount
+            print("Select an Expense Category:\n")
+            for cat in expense_category:
+                print(cat)
+            while True:
+
+                category_input = input("\nEnter Expense Category:\n").lower()
+                if category_input.replace(" ","").isalpha():
+                    category = category_input
+                    break
+
+                elif category_input != category_input.isalpha():
+                     print("Invalid Option")
+
+            amount = float(input("\nEnter an Amount:\n"))
+            expense[category] = amount
         elif option =="d":
             break
         else:
             print("Invalid Option")
     return expense
-# To Collect all  the related to tax
+# Tax calculator
 def taxes_collect():
     taxes = {}
+    tax_category =['1. Individual Income Tax','2. Corporate Tax','3. Capital Gain Tax','4. Import Duties','5. Sales Tax','6. Other Tax']
     while True:
-        option = input("Type 't' to add a tax rate or 'd' to finish:\n").lower()
+        option = input("\nType 't' to add a tax rate or 'd' to finish:\n").lower()
         if option == "t":
-            category = input("Enter Tax Category:\n")
-            rate = float(input("Enter Tax Rate (e.g., 10 for 10%):\n"))
+            print("Select a Tax Category:\n")
+            for cat in tax_category:
+                print(cat)
+            category = input("\nEnter Tax Category:\n")
+            rate = float(input("\nEnter Tax Rate (e.g., 10 for 10%):\n"))
             taxes[category] = rate  # always store as rate (%)
         elif option == "d":
             break
         else:
-            print("Invalid Option.")
+            print("Invalid Option. Please type 'tax' or 'done'.")
     return taxes
+
+
+
